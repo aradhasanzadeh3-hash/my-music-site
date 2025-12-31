1c3c103073367085a2caced01103f68e2f84684a
Arad <aradhasanzadeh3@gmail.com>
2025-12-31 23:27:42 +0330

    hello site

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.set_page_config(page_title="سلام", layout="centered")
 
-st.title("سلام 👋")
+st.title("Arad Music 🎵")
 st.write("این سایت تستی منه، بعداً آهنگ و دانلود اضافه می‌کنم 🎵")
 
